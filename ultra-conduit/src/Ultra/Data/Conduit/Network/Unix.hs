f7865a58c1289558a333d4ce8e87390ca84a35b9
Dom De Re <domdere@domdere.com>
2016-07-05 18:47:41 +1000

    exporting Data.Conduit.Network, Unix and UDP

---
+++ ultra-conduit/src/Ultra/Data/Conduit/Network/Unix.hs
@@ -0,0 +1,11 @@
+-------------------------------------------------------------------
+-- |
+-- Module       : Ultra.Data.Conduit.Network.Unix
+-- Copyright    : (C) 2016
+-- License      : BSD-style (see the file etc/LICENSE.md)
+-- Maintainer   : Dom De Re
+--
+-------------------------------------------------------------------
+module Ultra.Data.Conduit.Network.Unix (module X) where
+
+import Data.Conduit.Network.Unix as X
