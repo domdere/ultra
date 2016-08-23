69f7d1dfd0d7bb2b34df7686dd91b6531c57dd44
Dom De Re <domdere@domdere.com>
2016-08-23 23:03:59 +1000

    jsonTextEnums
    - Function added to `Lab.Core.Gen`
    - Prop test for succes
    - Prop test for fail

--- ultra-aeson/src/Ultra/Data/Aeson.hs
+++ ultra-aeson/src/Ultra/Data/Aeson.hs
@@ -9,10 +9,11 @@
 --
 -------------------------------------------------------------------
 module Ultra.Data.Aeson (
-        module X
-    -- * Functions
-    ,   checkVersion
-    ) where
+      module X
+  -- * Functions
+  , checkVersion
+  , jsonTextEnum
+  ) where
 
 import qualified Ultra.Data.Text as T
 
@@ -23,6 +24,19 @@ import Preamble
 
 checkVersion :: T.Text -> T.Text -> Parser a -> Parser a
 checkVersion v ev p =
-    let
-        failString = T.unpack . T.concat $ ["expected version '", ev, "' but found '", v, "'"]
-    in unless (v == ev) (fail failString) >> p
+  let
+    failString = T.unpack . T.concat $ ["expected version '", ev, "' but found '", v, "'"]
+  in unless (v == ev) (fail failString) >> p
+
+jsonTextEnum :: NonEmpty (T.Text, a) -> Value -> Parser a
+jsonTextEnum cases v =
+  let
+    errorText :: T.Text -> T.Text
+    errorText found = T.bracketedList "expected one of ['" (T.concat ["'], but found: '", found, "'"]) "', '" . toList . fmap fst $ cases
+  in do
+    t <- parseJSON v
+    foldr
+      (\(t', x) p -> if (t == t') then pure x else p)
+      (fail . T.unpack . errorText $ t)
+      cases
+
--- ultra-aeson/test/Test/Ultra/Data/Aeson.hs
+++ ultra-aeson/test/Test/Ultra/Data/Aeson.hs
@@ -13,7 +13,8 @@ module Test.Ultra.Data.Aeson where
 
 import Ultra.Data.Aeson
 
-import Lab.Core.QuickCheck
+import Lab.Core.Gen (distinctListOfN1, textOf1, alphaNumChars)
+import Lab.Core.QuickCheck (Arbitrary(..), Gen, Property, (===), (==>), choose, elements, forAll, suchThat)
 import Lab.Core.QuickCheck.TH (quickCheckAll)
 
 import qualified Ultra.Data.Text as T
@@ -49,6 +50,32 @@ prop_checkVersion_fail n v v' =
             ,   "'"
             ]
     in (v /= v') ==> (parseEither (p v v') j === (Left failString :: Either String Int))
+
+prop_jsonTextEnum_success :: Property
+prop_jsonTextEnum_success =
+  let
+    tags :: Gen (NonEmpty (T.Text, Int), T.Text, Int)
+    tags = do
+      listSize <- fromIntegral <$> choose (0, 39 :: Int)
+      ts <- distinctListOfN1 listSize (textOf1 alphaNumChars)
+      ts' <- forM ts $ \x -> (,) x <$> arbitrary
+      (testTag, expectedResult) <- elements . toList $ ts'
+      pure (ts', testTag, expectedResult)
+  in forAll tags $ \(tags', tagToCheck, expected) ->
+    parseEither (jsonTextEnum tags') (toJSON tagToCheck) === pure expected
+
+prop_jsonTextEnum_fail :: Property
+prop_jsonTextEnum_fail =
+  let
+    tags :: Gen (NonEmpty (T.Text, Int), T.Text)
+    tags = do
+      listSize <- fromIntegral <$> choose (0, 39 :: Int)
+      testTag <- textOf1 alphaNumChars
+      ts <- distinctListOfN1 listSize ((textOf1 alphaNumChars) `suchThat` (/= testTag))
+      ts' <- forM ts $ \x -> (,) x <$> arbitrary
+      pure (ts', testTag)
+  in forAll tags $ \(tags', tagToCheck) ->
+    parseMaybe (jsonTextEnum tags') (toJSON tagToCheck) === Nothing
 
 return []
 tests :: IO Bool
