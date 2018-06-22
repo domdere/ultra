6f16ed937c70270da7a8801c74be9098becd748b
Dom De Re <domdere@domdere.com>
2018-06-22 12:43:36 +1000

    ultra-unordered-containers
    - new project
    - mafia lock 8.2.2

---
+++ ultra-unordered-containers/src/Ultra/Data/HashMap/Lazy.hs
@@ -0,0 +1,11 @@
+-------------------------------------------------------------------
+-- |
+-- Module       : Ultra.Data.HashMap.Lazy
+-- Copyright    : (C) 2018
+-- License      : BSD-style (see the file /LICENSE.md)
+-- Maintainer   : Dom De Re
+--
+-------------------------------------------------------------------
+module Ultra.Data.HashMap.Lazy (module X) where
+
+import Data.HashMap.Lazy as X
---
+++ ultra-unordered-containers/src/Ultra/Data/HashSet.hs
@@ -0,0 +1,11 @@
+-------------------------------------------------------------------
+-- |
+-- Module       : Ultra.Data.HashSet
+-- Copyright    : (C) 2018
+-- License      : BSD-style (see the file etc/LICENSE.md)
+-- Maintainer   : Dom De Re
+--
+-------------------------------------------------------------------
+module Ultra.Data.HashSet (module X) where
+
+import Data.HashSet as X
