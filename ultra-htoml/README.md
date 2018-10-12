14bbc9d3c1837d401ae83c4b59744db49fe72ad2
Dom De Re <domdere@domdere.com>
2018-10-12 16:48:59 +1100

    ultra-htoml
    - basic parsers

---
+++ ultra-htoml/README.md
@@ -0,0 +1,7 @@
+# ultra-htoml
+
+HTOML is a much preferable config format than JSON, and YAML (its more well specced for things like strings etc...)
+
+However its library support for haskell is a little skint, this lib provides some
+parsing of the `htoml` types that is similar to `aeson` for JSON, as a bit of experiment
+on ways to extract information from the htoml types.
---
+++ ultra-htoml/src/Ultra/Text/Toml/Parser.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+-------------------------------------------------------------------
+-- |
+-- Module       : Ultra.Text.Toml.Parser
+-- Copyright    : (C) 2018
+-- License      : BSD-style (see the file /LICENSE.md)
+-- Maintainer   : Dom De Re
+--
+-------------------------------------------------------------------
+module Ultra.Text.Toml.Parser (module X) where
+
+import Text.Toml.Parser as X
